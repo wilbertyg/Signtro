f8e161636a68b9acc5bb026d42af430f2a0a4ff7
wilbert <ywilbert12@gmail.com>
2024-10-18 15:43:45 +0700

    --

---
+++ Signtro/src/Home.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './Navbar'
+
+function Home() {
+
+return (
+    <>
+        {/* <Navbar /> */}
+        <img src='./assets/illustration.svg' />
+    </>
+)
+}
+
+export default Home
--- Signtro/src/Navbar.jsx
+++ Signtro/src/Navbar.jsx
@@ -1,17 +1,27 @@
-import Logo from "./assets/logo.svg"; 
+import { Link } from 'react-router-dom';
+import Logo from "./assets/logo.svg";
+
+import "./Navbar.css";
 
 function Navbar() {
     return (
         <nav className="navbar">
-        <div className="nav-logo">
-        <img src={Logo} alt="Signtro" className="logo" />
-        </div>
-        <div className="nav-links">
-            <a href="#home">Home</a>
-            <a href="#courses">Courses</a>
-            <a href="#dictionary">Dictionary</a>
-        </div>
-    </nav>
+            <div className="nav-logo">
+                <Link to="/">
+                    {/* <img src={Logo} alt="Signtro" className="logo" /> */}
+                </Link>
+            </div>
+            <div className="nav-links">
+                {/* <Link to="/">Home</Link>
+                <Link to="/">Courses</Link>
+                <Link to="/">Dictionary</Link> */}
+            </div>
+            <div>
+                {/* <Link to="/profile" className="nav-profile">
+                    <img src={Profile} alt="Profile" className="profile" />
+                </Link> */}
+            </div>
+        </nav>
     );
 }
 
